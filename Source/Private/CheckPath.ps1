4a63f879f2cd035f83ff237b3b9420a62dc96e2f
Russell Slater <457324+russellds@users.noreply.github.com>
2022-03-30 16:18:38 -0400

    Feature/vm SKU report (#4)
    * Added git ignore
    
    * Enhancements to report.

--- Source/Private/CheckPath.ps1
+++ Source/Private/CheckPath.ps1
@@ -1,4 +1,5 @@
 function CheckPath {
+    [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
         [System.IO.FileInfo]
--- Source/Public/New-AzReportsPolicyAssignment.ps1
+++ Source/Public/New-AzReportsPolicyAssignment.ps1
@@ -22,7 +22,6 @@ function New-AzReportsPolicyAssignment {
         [System.IO.FileInfo]
         $Path,
 
-        [Parameter(Mandatory)]
         [string]
         $Name,
 
@@ -37,82 +36,65 @@ function New-AzReportsPolicyAssignment {
     $InformationPreference = 'Continue'
     $env:SuppressAzurePowerShellBreakingChangeWarnings = 'true'
 
-    CheckAzContext
+    try {
+        CheckAzContext
 
-    CheckPath -Path $Path -Extension '.xlsx' -Force:$Force
+        CheckPath -Path $Path -Extension '.xlsx' -Force:$Force -ErrorAction Stop
 
-    $policyAssignment = Get-AzPolicyAssignment -Name SecurityCenterBuiltIn
+        if ($Name) {
+            $policyAssignments = Get-AzPolicyAssignment -Name $Name
+        } else {
+            $policyAssignments = Get-AzPolicyAssignment
+        }
 
-    $policySetDefinition = Get-AzPolicySetDefinition -ResourceId $policyAssignment.Properties.PolicyDefinitionId
+        $objects = @()
 
-    $objects = @()
+        foreach ( $policyAssignment in $policyAssignments) {
+            if ($policyAssignment.Properties.Parameters) {
+                $objects += GetPolicyAssignmentParameters -PolicyAssignment $policyAssignment
+            } else {
+                Write-Information "Policy Assignment: $( $policyAssignment.Properties.DisplayName ) - has no parameters."
 
-    foreach ($policySetPolicyDefinition in $policySetDefinition.Properties.PolicyDefinitions) {
-        $parameters = $policySetPolicyDefinition.parameters.PSObject.Members |
-            Where-Object { $_.MemberType -eq 'NoteProperty' } |
-            Select-Object -ExpandProperty Name
-
-        $policyDefinition = Get-AzPolicyDefinition -ResourceId $policySetPolicyDefinition.policyDefinitionId
-
-        foreach ($parameter in $parameters) {
-            Write-Information "Policy: $($policyDefinition.Properties.DisplayName) Parameter: $parameter"
-
-            if ($policySetPolicyDefinition.parameters.$parameter.value.GetType().Name -eq 'String') {
-                if ($policySetPolicyDefinition.parameters.$parameter.value.StartsWith('[parameters')) {
-                    $item = [PSCustomObject]@{
-                        Name             = $policyDefinition.Properties.DisplayName
-                        Category         = $policyDefinition.Properties.Metadata.category
-                        'Parameter Name' = $($policySetPolicyDefinition.parameters.$parameter.value.Split("'"))[1]
-                        'Parameter Type' = $policyDefinition.Properties.Parameters.$parameter.type
-                        'Allowed Values' = $policyDefinition.Properties.Parameters.$parameter.allowedValues -join ', '
-                        'Default Value'  = $null
-                        'Desired Value'  = $null
-                    }
-
-                    if ($policyDefinition.Properties.Parameters.$parameter.type -eq 'Object') {
-                        $item.'Default Value' = $policyDefinition.Properties.Parameters.$parameter.defaultValue |
-                            ConvertTo-Json -Compress
-                    } else {
-                        $item.'Default Value' = $policyDefinition.Properties.Parameters.$parameter.defaultValue -join ', '
-                    }
-
-                    $objects += $item
+                $objects += [PSCustomObject]@{
+                    Name             = $policyAssignment.Name
+                    'Display Name'   = $policyAssignment.Properties.DisplayName
+                    Scope            = $policyAssignment.Properties.Scope
+                    'Parameter Name' = $null
+                    Value            = $null
                 }
             }
         }
+
+        $excelSplat = @{
+            Path          = $Path
+            WorksheetName = 'PolicyAssignment'
+            TableStyle    = 'Medium2'
+            AutoSize      = $true
+            FreezeTopRow  = $true
+            Style         = $excelStyle
+            PassThru      = $true
+        }
+
+        $excel = $objects |
+            Sort-Object -Property Category, Name |
+            Export-Excel @excelSplat
+
+        $workSheet = $excel.Workbook.Worksheets[$excelSplat.WorksheetName]
+
+        Set-ExcelRow -Worksheet $workSheet -Row 1 -Bold -HorizontalAlignment Center
+
+        Set-ExcelColumn -Worksheet $workSheet -Column 1 -AutoSize
+        Set-ExcelColumn -Worksheet $workSheet -Column 2 -AutoSize
+        Set-ExcelColumn -Worksheet $workSheet -Column 3 -AutoSize
+        Set-ExcelColumn -Worksheet $workSheet -Column 4 -AutoSize
+        Set-ExcelColumn -Worksheet $workSheet -Column 5 -AutoSize
+
+        if ($NoInvoke) {
+            Close-ExcelPackage -ExcelPackage $excel
+        } else {
+            Close-ExcelPackage -ExcelPackage $excel -Show
+        }
+    } catch {
+        throw $PSItem
     }
-
-    $excelSplat = @{
-        Path          = $Path
-        WorksheetName = $Name
-        TableStyle    = 'Medium2'
-        AutoSize      = $true
-        FreezeTopRow  = $true
-        Style         = $excelStyle
-        PassThru      = $true
-    }
-
-    $excel = $objects |
-        Sort-Object -Property Category, Name |
-        Export-Excel @excelSplat
-
-    $workSheet = $excel.Workbook.Worksheets[$excelSplat.WorksheetName]
-
-    Set-ExcelRow -Worksheet $workSheet -Row 1 -Bold -HorizontalAlignment Center
-
-    Set-ExcelColumn -Worksheet $workSheet -Column 1 -AutoSize
-    Set-ExcelColumn -Worksheet $workSheet -Column 2 -AutoSize
-    Set-ExcelColumn -Worksheet $workSheet -Column 3 -AutoSize
-    Set-ExcelColumn -Worksheet $workSheet -Column 4 -Width 19 -HorizontalAlignment Center
-    Set-ExcelColumn -Worksheet $workSheet -Column 5 -AutoSize
-    Set-ExcelColumn -Worksheet $workSheet -Column 6 -AutoSize -HorizontalAlignment Right
-    Set-ExcelColumn -Worksheet $workSheet -Column 7 -Width 18
-
-
-    if ($NoInvoke) {
-        Close-ExcelPackage -ExcelPackage $excel
-    } else {
-        Close-ExcelPackage -ExcelPackage $excel -Show
-    }
-
 }
