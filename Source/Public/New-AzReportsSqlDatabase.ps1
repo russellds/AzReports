623102953b1a8a353d9215be02c57c9488df213b
Russell Slater <457324+russellds@users.noreply.github.com>
2022-12-22 22:49:16 -0500

    Added Sql Servers Details

--- Source/Public/New-AzReportsSqlDatabase.ps1
+++ Source/Public/New-AzReportsSqlDatabase.ps1
@@ -1,4 +1,4 @@
-#requires -Modules ImportExcel
+#requires -Modules ImportExcel, Az.ResourceGraph
 
 function New-AzReportsSqlDatabase {
     <#
@@ -38,61 +38,44 @@ function New-AzReportsSqlDatabase {
 
         CheckPath -Path $Path -Extension '.xlsx' -Force:$Force -ErrorAction Stop
 
-        $query = @'
+        $serverQuery = @'
 resources
-| where type == "microsoft.network/trafficmanagerprofiles"
-| order by subscriptionId asc , resourceGroup asc , name asc
+| where type == "microsoft.sql/servers"
+| join kind=inner (
+    resourcecontainers
+    | where type == 'microsoft.resources/subscriptions'
+    | project subscriptionId, subscriptionName = name)
+    on subscriptionId
+| project-away subscriptionId1
 '@
 
 
-        $queryResults = SearchAzGraph -Query $query
+        $serverQueryResults = SearchAzGraph -Query $serverQuery
 
-        $tmProfiles = [System.Collections.ArrayList]::new()
-        $tmEndpoints = [System.Collections.ArrayList]::new()
+        $sqlServers = [System.Collections.ArrayList]::new()
 
-        foreach ($queryResult in $queryResults) {
-            $null = $tmProfiles.Add([PSCustomObject]@{
-                    'Subscription Id'                      = $queryResult.subscriptionId
-                    'Resource Group'                       = $queryResult.resourceGroup
-                    Name                                   = $queryResult.name
-                    Status                                 = $queryResult.properties.profileStatus
-                    'Routing Method'                       = $queryResult.properties.TrafficRoutingMethod
-                    'Max Return'                           = $queryResult.properties.MaxReturn
-                    'Traffic View Enrollent'               = $queryResult.properties.trafficViewEnrollmentStatus
-                    FQDN                                   = $queryResult.properties.dnsConfig.fqdn
-                    'Relative DNS Name'                    = $queryResult.properties.dnsConfig.relativeName
-                    TTL                                    = $queryResult.properties.dnsConfig.TTL
-                    'Monitor Status'                       = $queryResult.properties.monitorConfig.profileMonitorStatus
-                    'Monitor Protocol'                     = $queryResult.properties.monitorConfig.protocol
-                    'Monitor Port'                         = $queryResult.properties.monitorConfig.port
-                    'Monitor Path'                         = $queryResult.properties.monitorConfig.path
-                    'Monitor Interval in Seconds'          = $queryResult.properties.monitorConfig.intervalInSeconds
-                    'Monitor Tolerated Number of Failures' = $queryResult.properties.monitorConfig.toleratedNumberOfFailures
-                    'Monitor Timeout in Seconds'           = $queryResult.properties.monitorConfig.timeoutInSeconds
+        foreach ($serverQueryResult in $serverQueryResults) {
+            $null = $sqlServers.Add([PSCustomObject]@{
+                    'Subscription Id'                  = $serverQueryResult.subscriptionId
+                    'Subscription Name'                = $serverQueryResult.subscriptionName
+                    'Resource Group'                   = $serverQueryResult.resourceGroup
+                    Name                               = $serverQueryResult.name
+                    Location                           = $serverQueryResult.location
+                    'Administrator Login'              = $serverQueryResult.properties.administratorLogin
+                    'AAD Administrator'                = $serverQueryResult.properties.administrators.login
+                    'FQDN'                             = $serverQueryResult.properties.fullyQualifiedDomainName
+                    Kind                               = $serverQueryResult.kind
+                    'Public Network Access'            = $serverQueryResult.properties.publicNetworkAccess
+                    #'Private Endpoint Connections'     = $serverQueryResult.privateEndpointConnections.ToString()
+                    'Restrict Outbound Network Access' = $serverQueryResult.properties.restrictOutboundNetworkAccess
+                    State                              = $serverQueryResult.properties.state
+                    Version                            = $serverQueryResult.properties.version
                 })
-
-            foreach ($endpoint in $queryResult.properties.endpoints) {
-                $null = $tmEndpoints.Add([PSCustomObject]@{
-                        'Subscription Id'          = $queryResult.subscriptionId
-                        'Resource Group'           = $queryResult.resourceGroup
-                        'Profile Name'             = $queryResult.name
-                        'Endpoint Name'            = $endpoint.name
-                        Status                     = $endpoint.properties.endpointStatus
-                        Target                     = $endpoint.properties.target
-                        Priority                   = $endpoint.properties.priority
-                        Weight                     = $endpoint.properties.weight
-                        Location                   = $endpoint.properties.endpointLocation
-                        'Min Child Endpoints'      = $endpoint.properties.minChildEndpoints
-                        'Min Child Endpoints IPv4' = $endpoint.properties.minChildEndpointsIPv4
-                        'Min Child Endpoints IPv6' = $endpoint.properties.minChildEndpointsIPv6
-
-                    })
-            }
         }
 
         $excelSplat = @{
             Path          = $Path
-            WorksheetName = 'Profiles'
+            WorksheetName = 'SQL Servers'
             TableStyle    = 'Medium2'
             AutoSize      = $true
             FreezeTopRow  = $true
@@ -100,7 +83,8 @@ resources
             PassThru      = $true
         }
 
-        $excel = $tmProfiles |
+        $excel = $sqlServers |
+            Sort-Object -Property 'Subscription Id', 'Resource Group', Name |
             Export-Excel @excelSplat
 
         $workSheet = $excel.Workbook.Worksheets[$excelSplat.WorksheetName]
@@ -116,45 +100,11 @@ resources
         Set-ExcelColumn -Worksheet $workSheet -Column 7 -AutoSize
         Set-ExcelColumn -Worksheet $workSheet -Column 8 -AutoSize
         Set-ExcelColumn -Worksheet $workSheet -Column 9 -AutoSize
-        Set-ExcelColumn -Worksheet $workSheet -Column 10 -AutoSize -HorizontalAlignment Center
+        Set-ExcelColumn -Worksheet $workSheet -Column 10 -AutoSize
         Set-ExcelColumn -Worksheet $workSheet -Column 11 -AutoSize
         Set-ExcelColumn -Worksheet $workSheet -Column 12 -AutoSize
-        Set-ExcelColumn -Worksheet $workSheet -Column 13 -AutoSize -HorizontalAlignment Center
+        Set-ExcelColumn -Worksheet $workSheet -Column 13 -AutoSize
         Set-ExcelColumn -Worksheet $workSheet -Column 14 -AutoSize
-        Set-ExcelColumn -Worksheet $workSheet -Column 15 -AutoSize -HorizontalAlignment Center
-        Set-ExcelColumn -Worksheet $workSheet -Column 16 -AutoSize -HorizontalAlignment Center
-        Set-ExcelColumn -Worksheet $workSheet -Column 17 -AutoSize -HorizontalAlignment Center
-
-        $excelSplat = @{
-            ExcelPackage  = $excel
-            WorksheetName = 'Endpoints'
-            TableStyle    = 'Medium2'
-            AutoSize      = $true
-            FreezeTopRow  = $true
-            Style         = $excelStyle
-            PassThru      = $true
-        }
-
-        $null = $tmEndpoints |
-            Sort-Object -Property 'Subscription Id', 'Resource Group', 'Profile Name', 'Endpoint Name' |
-            Export-Excel @excelSplat
-
-        $workSheet = $excel.Workbook.Worksheets[$excelSplat.WorksheetName]
-
-        Set-ExcelRow -Worksheet $workSheet -Row 1 -Bold -HorizontalAlignment Center
-
-        Set-ExcelColumn -Worksheet $workSheet -Column 1 -AutoSize
-        Set-ExcelColumn -Worksheet $workSheet -Column 2 -AutoSize
-        Set-ExcelColumn -Worksheet $workSheet -Column 3 -AutoSize
-        Set-ExcelColumn -Worksheet $workSheet -Column 4 -AutoSize
-        Set-ExcelColumn -Worksheet $workSheet -Column 5 -AutoSize
-        Set-ExcelColumn -Worksheet $workSheet -Column 6 -AutoSize
-        Set-ExcelColumn -Worksheet $workSheet -Column 7 -AutoSize -HorizontalAlignment Center
-        Set-ExcelColumn -Worksheet $workSheet -Column 8 -AutoSize -HorizontalAlignment Center
-        Set-ExcelColumn -Worksheet $workSheet -Column 9 -AutoSize
-        Set-ExcelColumn -Worksheet $workSheet -Column 10 -AutoSize -HorizontalAlignment Center
-        Set-ExcelColumn -Worksheet $workSheet -Column 11 -AutoSize -HorizontalAlignment Center
-        Set-ExcelColumn -Worksheet $workSheet -Column 12 -AutoSize -HorizontalAlignment Center
 
         if ($NoInvoke) {
             Close-ExcelPackage -ExcelPackage $excel
